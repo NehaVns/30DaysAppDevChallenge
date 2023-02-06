f5c62bfdc362627e0b621a02c596691af144f331
NehaVns <nehavns2848@gmail.com>
2023-02-06 22:24:05 +0530

    Day 3 FirstDemoApp Version 1.0

--- FirstDemoApp/app/src/main/java/com/androidtutorials/firstdemoapp/MainActivity.kt
+++ FirstDemoApp/app/src/main/java/com/androidtutorials/firstdemoapp/MainActivity.kt
@@ -2,10 +2,21 @@ package com.androidtutorials.firstdemoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val buttonUpload = findViewById<Button>(R.id.btnUpload)
+        val buttonDownload = findViewById<Button>(R.id.btnDownload)
+        buttonUpload.setOnClickListener {
+            Toast.makeText(applicationContext, "Uploading...", Toast.LENGTH_SHORT).show()
+        }
+        buttonDownload.setOnClickListener {
+            Toast.makeText(applicationContext, "Downloading...", Toast.LENGTH_SHORT).show()
+        }
     }
+
 }
