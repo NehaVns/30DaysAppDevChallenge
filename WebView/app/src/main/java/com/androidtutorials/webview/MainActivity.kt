312e219dc6ed7b976eb5c1d86258e0b41b30f417
NehaVns <nehavns2848@gmail.com>
2023-02-11 19:27:30 +0530

    Day 11 WebView version 1.0

--- WebView/app/src/main/java/com/androidtutorials/webview/MainActivity.kt
+++ WebView/app/src/main/java/com/androidtutorials/webview/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.androidtutorials.webview
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.annotation.RequiresApi
+
+
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val webView = findViewById<WebView>(R.id.webView)
+
+        webViewSetUp(webView)
+
+    }
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun webViewSetUp(webView :WebView){
+        webView.webViewClient = WebViewClient()
+        webView.apply{
+            settings.javaScriptEnabled = true
+            settings.safeBrowsingEnabled = true
+            loadUrl("https://medium.com/@nehavns2848")
+
+
+        }
+
     }
 }
