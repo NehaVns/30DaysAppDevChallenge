6b093dbc8b086e9b8a59aa811ca3106d2f5c405f
NehaVns <nehavns2848@gmail.com>
2023-02-12 21:41:16 +0530

    Day 12 Assignment version 1.1

--- KnowledgeHub/app/src/main/java/com/androidtutorials/knowledgehub/CardsActivity.kt
+++ KnowledgeHub/app/src/main/java/com/androidtutorials/knowledgehub/CardsActivity.kt
@@ -1,11 +1,21 @@
 package com.androidtutorials.knowledgehub
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
 
 class CardsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cards)
+        val callButton = findViewById<Button>(R.id.btnCall)
+        callButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_DIAL)
+            intent.data = Uri.parse("tel:" + "**")
+            startActivity(intent)
+        }
     }
 }
