7acb9491189c7e4b9b886f47b6a0e17b48c22cb1
NehaVns <nehavns2848@gmail.com>
2023-02-14 19:16:54 +0530

    Day 13 My Cafe version 1.0

---
+++ MyCafe/app/src/main/java/com/androidtutorials/mycafe/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.androidtutorials.mycafe
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    //creating key
+    companion object{
+        const val KEY = "com.androidtutorials.mycafe.MainActivity.KEY"
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnOrder.setOnClickListener {
+            val orderList = eT1.text.toString()+","+eT2.text.toString()+","+eT3.text.toString()+","+eT4.text.toString()
+
+            intent = Intent(this, ShowOrder::class.java)
+            intent.putExtra(KEY, orderList)
+            startActivity(intent)
+
+        }
+    }
+}
---
+++ MyCafe/app/src/main/java/com/androidtutorials/mycafe/ShowOrder.kt
@@ -0,0 +1,14 @@
+package com.androidtutorials.mycafe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_show_order.*
+
+class ShowOrder : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_order)
+        val orderOfCustomer = intent.getStringArrayExtra(MainActivity.KEY)
+        tvOrder.text = orderOfCustomer.toString()
+    }
+}
