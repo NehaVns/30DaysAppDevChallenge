d0794ae17fa3311fadd4ca0258c7851ac6b75334
NehaVns <nehavns2848@gmail.com>
2023-02-15 20:14:26 +0530

    Day 14 Splash Screen version 1.0

--- SplashScreen/app/src/main/java/com/androidtutorials/splashscreen/MainActivity.kt
+++ SplashScreen/app/src/main/java/com/androidtutorials/splashscreen/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.androidtutorials.splashscreen
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
     }
 }
---
+++ SplashScreen/app/src/main/java/com/androidtutorials/splashscreen/MainActivity2.kt
@@ -0,0 +1,12 @@
+package com.androidtutorials.splashscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+    }
+}
