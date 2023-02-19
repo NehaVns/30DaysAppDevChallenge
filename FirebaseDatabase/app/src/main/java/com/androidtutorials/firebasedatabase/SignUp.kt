5ff42d1555fa6e06f614f00da69b830596be5dd1
NehaVns <nehavns2848@gmail.com>
2023-02-19 15:27:04 +0530

    Day 16 Firebase Database version 1.1

--- FirebaseDatabase/app/src/main/java/com/androidtutorials/firebasedatabase/SignUp.kt
+++ FirebaseDatabase/app/src/main/java/com/androidtutorials/firebasedatabase/SignUp.kt
@@ -3,22 +3,40 @@ package com.androidtutorials.firebasedatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class SignUp : AppCompatActivity() {
+
+    lateinit var database : DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
 
         val signupButton = findViewById<Button>(R.id.btnSignup)
-        val edU_Name = findViewById<EditText>(R.id.edUserName)
-        val edU_email = findViewById<EditText>(R.id.edMail)
-        val edU_password = findViewById<EditText>(R.id.edPassword)
+        val edUName = findViewById<TextInputEditText>(R.id.edUserName)
+        val edUEmail = findViewById<TextInputEditText>(R.id.edMail)
+        val edUPassword = findViewById<TextInputEditText>(R.id.edPassword)
 
-        signupButton.setOnClickListener{
-            val userName = edU_Name.text
-            val user_email = edU_email.text
-            val user_password = edU_password.text
+        signupButton.setOnClickListener {
+            val userName = edUName.text.toString()
+            val userEmail = edUEmail.text.toString()
+            val userPassword = edUPassword.text.toString()
+
+            val user = User(userName, userEmail, userPassword)
+            database = FirebaseDatabase.getInstance().getReference("Users")
+            database.child(userName).setValue(user).addOnSuccessListener {
+                edUName.text?.clear()
+                edUEmail.text?.clear()
+                edUPassword.text?.clear()
+
+                Toast.makeText(this, "User Registered", Toast.LENGTH_SHORT).show()
+            }.addOnFailureListener {
+                Toast.makeText(this, "Registration Failed", Toast.LENGTH_SHORT).show()
+            }
+
         }
 
     }
---
+++ FirebaseDatabase/app/src/main/java/com/androidtutorials/firebasedatabase/User.kt
@@ -0,0 +1,5 @@
+package com.androidtutorials.firebasedatabase
+
+data class User(val name : String, val email : String, val password : String){
+
+}
