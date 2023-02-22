74fc12e5933aa670d49916b9a2bed16a24061a52
NehaVns <nehavns2848@gmail.com>
2023-02-22 06:56:40 +0530

    Day 17 firebase database version 1.3

--- FirebaseDatabase/app/src/main/java/com/androidtutorials/firebasedatabase/Login.kt
+++ FirebaseDatabase/app/src/main/java/com/androidtutorials/firebasedatabase/Login.kt
@@ -2,10 +2,33 @@ package com.androidtutorials.firebasedatabase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.database.DatabaseReference
 
 class Login : AppCompatActivity() {
+    lateinit var databaseReference: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        val signInButton = findViewById<Button>(R.id.btnSignIn)
+        val userNameDatabase = findViewById<TextInputEditText>(R.id.UserName)
+
+        signInButton.setOnClickListener{
+            //take reference till node "Users".
+            val userNameString = userNameDatabase.text.toString()
+            if(userNameString.isNotEmpty()){
+                readData(userNameString)
+            }else{
+                Toast.makeText(this, "Username can't be empty.", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
     }
+    private fun readData(userNameForDatabase : String){
+
+    }
+
 }
